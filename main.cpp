ca50582fa70b927e222a644b052288b5085c3824
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-11-10 15:18:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ int main(){
     float b = 0;
     float c = 0;
     cin >> a >> b >> c;
-    std::cout<<"Area triangolo: "<<(a*b)/2<<std::endl;
-    std::cout<<"Area quadrato: "<<a*a<<std::endl;
-    std::cout<<"Area rettangolo: "<<a*b<<std::endl;
-    std::cout<<"Area trapezio: "<<((a+b)*c)/2<<std::endl;
+    std::cout<<(a*b)/2<<std::endl;
+    std::cout<<a*a<<std::endl;
+    std::cout<<a*b<<std::endl;
+    std::cout<<((a+b)*c)/2<<std::endl;
     return 0;
 }
