9beff94a7c52662889ca63425f3b07dc5ecf1693
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-11-10 15:02:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    float a = 0;
+    float b = 0;
+    float c = 0;
+    cout<<"Inserire il valore di a, b e c: "<<endl;
+    cin >> a >> b >> c;
+    cout<<"Area triangolo: "<<(a*b)/2<<endl;
+    cout<<"Area quadrato: "<<pow(a,2)<<endl;
+    cout<<"Area rettangolo: "<<a*b<<endl;
+    cout<<"Area trapezio: "<<((a+b)*c)/2<<endl;
+    return 0;
 }
