ccdf4c3c13891c5af585b293c399efa0af5575ea
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-11-10 15:45:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 using namespace std;
 
 int main(){
-    float a;
-    float b;
-    float c;
-    a=0;
-    b=0;
-    c=0;
+    float a=0;
+    float b=0;
+    float c=0;
     cin >> a >> b >> c;
     cout<<(a*b)*0.5<<std::endl;
     cout<<pow(a,2)<<std::endl;
