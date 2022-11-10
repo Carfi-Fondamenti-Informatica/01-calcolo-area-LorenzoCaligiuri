a297e5df625534b648942f212ce01ffe364d479f
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-11-10 15:36:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 int main(){
-    float a = 0;
-    float b = 0;
-    float c = 0;
-    cin >> a >> b >> c;
-    std::cout<<(a*b)/2<<std::endl;
-    std::cout<<a*a<<std::endl;
-    std::cout<<a*b<<std::endl;
-    std::cout<<((a+b)*c)/2<<std::endl;
+    float a;
+    float b;
+    float c;
+    a=0;
+    b=0;
+    c=0;
+    std::cin >> a >> b >> c;
+    std::cout<<(a*b)*0.5<<std::endl;
+    std::cout<<(a*a)<<std::endl;
+    std::cout<<(a*b)<<std::endl;
+    std::cout<<((a+b)*c)*0.5<<std::endl;
     return 0;
 }
